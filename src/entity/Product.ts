4239cfb9424838d1c069b724a4ade186cfd1df40
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-01-15 12:00:51 +0530

    add: product entity

---
+++ src/entity/Product.ts
@@ -0,0 +1,51 @@
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    ManyToMany,
+    OneToMany,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+} from "typeorm";
+import { Category, SizeAndPrice } from "./";
+
+@Entity({ name: "products" })
+class Product {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column("text")
+    description: string;
+
+    @Column("boolean")
+    availability: boolean;
+
+    @Column("text")
+    imageUrl: string;
+
+    @Column()
+    preparationTime: number;
+
+    @Column()
+    discount: number;
+
+    @Column("simple-array")
+    ingredients: string[];
+
+    @ManyToMany(() => Category)
+    categories: Category[];
+
+    @OneToMany(() => SizeAndPrice, (sizeAndPrice) => sizeAndPrice.id)
+    sizeAndPrice: SizeAndPrice[];
+
+    @UpdateDateColumn()
+    updatedAt: number;
+
+    @CreateDateColumn()
+    createdAt: number;
+}
+
+export default Product;
