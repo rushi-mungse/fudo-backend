2ec95a35a031f175c772eef47b9352f8b85a0e54
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-01-15 12:01:29 +0530

    add: user entity

---
+++ src/entity/User.ts
@@ -0,0 +1,51 @@
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    OneToMany,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+} from "typeorm";
+import { UserRole, UserStatus } from "../constants";
+import { Shipping } from "./";
+
+@Entity({ name: "users" })
+class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column("text")
+    fullName: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column({ type: "enum", enum: UserRole, default: UserRole.CUSTOMER })
+    role: string;
+
+    @Column({ nullable: true, default: null })
+    phoneNumber: string;
+
+    @Column({ nullable: true, default: null })
+    countryCode: string;
+
+    @Column("text", { nullable: true, default: null })
+    avatar: string;
+
+    @Column({ type: "enum", enum: UserStatus, default: UserStatus.VALID })
+    status: string;
+
+    @OneToMany(() => Shipping, (shipping) => shipping.id)
+    shippings: Shipping[];
+
+    @UpdateDateColumn()
+    updatedAt: number;
+
+    @CreateDateColumn()
+    createdAt: number;
+}
+
+export default User;
