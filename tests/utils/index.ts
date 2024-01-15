6f4107eacc65cb6a7f2e0ca0e6e10ed8a00e279f
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-01-15 23:58:59 +0530

    add: check valid jwt and get token methods

---
+++ tests/utils/index.ts
@@ -0,0 +1,36 @@
+// to check jwt token is valid
+export const isJWT = (token: string | null): boolean => {
+    if (token === null) return false;
+    const parts = token.split(".");
+
+    if (parts.length !== 3) return false;
+
+    try {
+        parts.forEach((part) => {
+            Buffer.from(part, "base64").toString("utf-8");
+        });
+        return true;
+    } catch (error) {
+        return false;
+    }
+};
+
+export const getTokens = (
+    response: Response,
+): { accessToken: string | null; refreshToken: string | null } => {
+    var accessToken: string | null = null;
+    let refreshToken: string | null = null;
+
+    const cookies =
+        (response.headers as Record<string, any>)["set-cookie"] || [];
+
+    cookies.forEach((cookie: string) => {
+        if (cookie.startsWith("accessToken"))
+            accessToken = cookie.split(";")[0].split("=")[1];
+
+        if (cookie.startsWith("refreshToken"))
+            refreshToken = cookie.split(";")[0].split("=")[1];
+    });
+
+    return { accessToken, refreshToken };
+};
