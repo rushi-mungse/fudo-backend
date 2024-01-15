245ed2b3a1ba614bb51405b204ddb1f488381c26
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-01-15 12:01:55 +0530

    add: all entities in index file

---
+++ src/entity/index.ts
@@ -0,0 +1,9 @@
+export { default as User } from "./User";
+export { default as Shipping } from "./Shipping";
+export { default as Token } from "./Token";
+export { default as Category } from "./Category";
+export { default as SizeAndPrice } from "./SizeAndPrice";
+export { default as Product } from "./Product";
+export { default as Order } from "./Order";
+export { default as OrderItem } from "./OrderItem";
+export { default as Payment } from "./Payment";
