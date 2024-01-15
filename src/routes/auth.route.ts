aa5fe602da92a48bbcab8d2a7cd480ecccce2682
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-01-15 23:55:22 +0530

    add: auth router

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,4 +1,50 @@
-import express from "express";
+import express, {
+    NextFunction,
+    Request,
+    RequestHandler,
+    Response,
+} from "express";
+import { AuthController } from "../controllers";
+import { sendOtpValidator, verifyOtpValidator } from "../validators/auth";
+import { SendOtpRequest, VerifyOtpRequest } from "../types";
+import { CredentialService, TokenService, UserService } from "../services";
+import { AppDataSource, logger } from "../config";
+import { Token, User } from "../entity";
+
 const router = express.Router();
+
+const userRepository = AppDataSource.getRepository(User);
+const tokenRepository = AppDataSource.getRepository(Token);
+const userService = new UserService(userRepository);
+const tokenService = new TokenService(tokenRepository);
+const credentialService = new CredentialService();
+const authController = new AuthController(
+    logger,
+    userService,
+    credentialService,
+    tokenService,
+);
+
+router.post(
+    "/register/send-otp",
+    sendOtpValidator,
+    (req: Request, res: Response, next: NextFunction) =>
+        authController.sendOtp(
+            req as SendOtpRequest,
+            res,
+            next,
+        ) as unknown as RequestHandler,
+);
+
+router.post(
+    "/register/verify-otp",
+    verifyOtpValidator,
+    (req: Request, res: Response, next: NextFunction) =>
+        authController.verifyOtp(
+            req as VerifyOtpRequest,
+            res,
+            next,
+        ) as unknown as RequestHandler,
+);
 
 export default router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,1 +1,5 @@
 export { default as authRouter } from "./auth.route";
+export { default as userRouter } from "./user.route";
+export { default as productRouter } from "./product.route";
+export { default as categoryRouter } from "./category.route";
+export { default as orderRouter } from "./order.route";
