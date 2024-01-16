fd1872f4ae9b2315f6d3d8bd2a10abb67b11d870
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-01-16 13:03:04 +0530

    add: check refresh token middleware

---
+++ src/middlewares/checkRefreshToken.middleware.ts
@@ -0,0 +1,34 @@
+import { Request } from "express";
+import { expressjwt } from "express-jwt";
+import createHttpError from "http-errors";
+import { CookieType, JWTPayload } from "../types";
+import { AppDataSource, logger, REFRESH_TOKEN_SECRET } from "../config";
+import { Token } from "../entity";
+
+export default expressjwt({
+    secret: REFRESH_TOKEN_SECRET ?? "",
+    algorithms: ["HS256"],
+    getToken(req: Request) {
+        const { refreshToken } = req.cookies as CookieType;
+        return refreshToken;
+    },
+    async isRevoked(req: Request, token) {
+        if (!token) return true;
+        const payload = token.payload as JWTPayload;
+        try {
+            const tokenRepository = AppDataSource.getRepository(Token);
+            const tokenRef = await tokenRepository.findOne({
+                where: {
+                    id: Number(payload.tokenId),
+                    user: { id: Number(payload.userId) },
+                },
+            });
+            return tokenRef === null;
+        } catch (error) {
+            logger.error("Error while getting the refresh token", {
+                userId: payload.userId,
+            });
+            return true;
+        }
+    },
+});
