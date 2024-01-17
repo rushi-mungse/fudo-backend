954641e076745a7efba20be39cb801e4c7a61bf4
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-01-17 08:54:30 +0530

    add: user controller

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,44 @@
+import { Logger } from "winston";
+import { Response, NextFunction } from "express";
+import { validationResult } from "express-validator";
+import createHttpError from "http-errors";
+import { CredentialService, UserService } from "../services";
+import { UpdateUserFullNameRequest } from "../types";
+
+class UserController {
+    constructor(
+        private logger: Logger,
+        private userService: UserService,
+        private credentialService: CredentialService,
+    ) {}
+
+    async updateUserFullName(
+        req: UpdateUserFullNameRequest,
+        res: Response,
+        next: NextFunction,
+    ) {
+        const result = validationResult(req);
+        if (!result.isEmpty())
+            return res.status(400).json({ error: result.array() });
+
+        const userId = req.auth.userId;
+        const { fullName } = req.body;
+
+        try {
+            const user = await this.userService.findUserById(Number(userId));
+            if (!user) return next(createHttpError(400, "User not found!"));
+
+            user.fullName = fullName;
+            await this.userService.saveUser(user);
+
+            return res.json({
+                message: "Updated user full name successfully.",
+                user,
+            });
+        } catch (error) {
+            return next(error);
+        }
+    }
+}
+
+export default UserController;
