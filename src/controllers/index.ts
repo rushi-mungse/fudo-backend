c4450b436c48b8f41cdc50c13c1f4028341dbf0a
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-01-18 15:29:30 +0530

    🔥 add: category controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,3 +1,4 @@
 export { default as AuthController } from "./auth.controller";
 export { default as UserController } from "./user.controller";
 export { default as ShippingController } from "./shipping.controller";
+export { default as CategoryController } from "./category.controller";
