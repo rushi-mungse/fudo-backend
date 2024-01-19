616557bc0a60956293813fa5385b5f9b89256c17
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-01-20 01:49:47 +0530

    add: body-parser and order route

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,30 @@
 import "reflect-metadata";
 import express from "express";
 import { errorHandlerMiddleware } from "./middlewares";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 import {
     authRouter,
     categoryRouter,
     productRouter,
     shippingRouter,
     userRouter,
+    orderRouter,
 } from "./routes";
-import cookieParser from "cookie-parser";
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static("public"));
-app.use(express.json());
 
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/shipping", shippingRouter);
 app.use("/api/category", categoryRouter);
 app.use("/api/product", productRouter);
+app.use("/api/order", orderRouter);
 
 app.use(errorHandlerMiddleware);
 
