13de00765456ea5307b7f0f22673fdb465cff61b
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-01-20 01:51:46 +0530

    add: order router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,3 +3,4 @@ export { default as userRouter } from "./user.route";
 export { default as shippingRouter } from "./shipping.route";
 export { default as categoryRouter } from "./category.route";
 export { default as productRouter } from "./product.route";
+export { default as orderRouter } from "./order.route";
