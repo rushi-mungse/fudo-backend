079d0847c97c084e5613d310c230a31c7c60848c
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-01-20 19:37:49 +0530

    add : many-to-many relation with product

--- src/entity/Category.ts
+++ src/entity/Category.ts
@@ -2,14 +2,19 @@ import {
     Column,
     CreateDateColumn,
     Entity,
+    ManyToMany,
     PrimaryGeneratedColumn,
     UpdateDateColumn,
 } from "typeorm";
+import Product from "./Product";
 
 @Entity({ name: "categories" })
 class Category {
     @PrimaryGeneratedColumn()
     id: number;
+
+    @ManyToMany(() => Product, (product) => product.category)
+    product: Product[];
 
     @Column()
     name: string;
