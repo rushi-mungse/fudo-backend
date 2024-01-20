063805be75ac794b197dff5c01fb69a92ae1424a
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-01-20 10:22:03 +0530

    add: many-to-one relation with product

--- src/entity/SizeAndPrice.ts
+++ src/entity/SizeAndPrice.ts
@@ -2,15 +2,20 @@ import {
     Column,
     CreateDateColumn,
     Entity,
+    ManyToOne,
     PrimaryGeneratedColumn,
     UpdateDateColumn,
 } from "typeorm";
 import { ProductSize } from "../constants";
+import Product from "./Product";
 
 @Entity({ name: "sizesAndPrices" })
 class SizeAndPrice {
     @PrimaryGeneratedColumn()
     id: number;
+
+    @ManyToOne(() => Product, (product) => product.sizeAndPrices)
+    product: Product;
 
     @Column({ type: "enum", enum: ProductSize })
     size: string;
