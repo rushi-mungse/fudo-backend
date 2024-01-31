1c0f8c56ed69a43f654e18ceb4d4d0a597ad94b5
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-01-31 18:42:26 +0530

    add: delete product size method

--- src/controllers/ProductSize.ts
+++ src/controllers/ProductSize.ts
@@ -6,6 +6,7 @@ import {
     ProductSizeData,
     ProductSizeServiceType,
 } from "../types/type";
+import createHttpError from "http-errors";
 
 class SizeController {
     constructor(
@@ -21,6 +22,30 @@ class SizeController {
         try {
             const productSize = await this.sizeService.save({ size });
             return res.json({ productSize, message: "Product size added!" });
+        } catch (error) {
+            return next(error);
+        }
+    }
+
+    async deleteProductSize(
+        req: AuthRequest,
+        res: Response,
+        next: NextFunction,
+    ) {
+        const productSizeId = req.params.productSizeId;
+        if (isNaN(Number(productSizeId)))
+            return next(createHttpError(400, "Invalid product size id!"));
+
+        try {
+            const size = await this.sizeService.getById(Number(productSizeId));
+            if (!size)
+                return next(createHttpError(400, "Product size not found!"));
+
+            await this.sizeService.delete(Number(productSizeId));
+            return res.json({
+                message: "Product size deleted successfully.",
+                id: productSizeId,
+            });
         } catch (error) {
             return next(error);
         }
