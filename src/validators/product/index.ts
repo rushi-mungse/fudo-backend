19469be889b2e13dc6f55e54b7710682d7ea1974
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-01-31 18:46:02 +0530

    add: validator

--- src/validators/product/index.ts
+++ src/validators/product/index.ts
@@ -1,2 +1,3 @@
 export { default as createProductValidator } from "./createProduct.validator";
 export { default as updateProductValidator } from "./updateProduct.validator";
+export { default as listProductValidator } from "./listProduct.validator";
